093e89bde1f554aaf2e2b235adef1f79f4a47a2d
elliott dombrowski <elliottroyal@me.com>
2022-04-26 13:19:34 -0500

    testing on pc

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const lottoNum = document.getElementById('lotto-num');
-
+const max = 100000;
 
 const cycleLottoNumbers = () => {
-}
+  lottoNum.innerHTML = Math.floor(Math.random() * max);
+  lottoNum.innerHTML > 1000 ? alert("Too bad! You've lost!") : alert("Congratulations! You've won!");
+};
