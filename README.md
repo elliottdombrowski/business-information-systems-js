d687e4a7f5aebf298578becf863735fb9916c726
elliott dombrowski <elliottroyal@me.com>
2022-04-26 15:05:11 -0500

    finished modal and wrote readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # business-information-systems-js
+
+
+# This is a simple Lottery game built with JavaScript.
+## When the user clicks the "Let's Play!" button, the program will filter through 10 random numbers between 1 and 100,000. 
+## If the final number is between 1 and 1000, the user will be presented with a prompt confirming that they have won.
+## Otherwise, the user will be presented with a prompt confirming that they have lost.
+
+---
+
+# NOTES
+
+### While recognizing that the example's modal appears to be a JavaScript alert, I chose to recreate the modal independently, as the example's alert does not match the current JS Alert appearance. 
+### I designed a modal similar to the provided example, and display / hide via JavaScript styles.
+
+---
+
+# GH PAGES DEPLOYMENT
--- index.js
+++ index.js
@@ -1,7 +1,45 @@
+// GRAB HTML ELEMENTS BY ID 
 const lottoNum = document.getElementById('lotto-num');
+const modal = document.getElementById('results-modal');
+const result = document.getElementById('result');
+
+//DECLARE MAX MULTIPLE FOR MATH.RANDOM
 const max = 100000;
 
+//DECLARE VARIABLE TO STORE MOST RECENTLY GENERATED NUMBER
+let latestNum = 0;
+
 const cycleLottoNumbers = () => {
-  lottoNum.innerHTML = Math.floor(Math.random() * max);
-  lottoNum.innerHTML > 1000 ? alert("Too bad! You've lost!") : alert("Congratulations! You've won!");
+  //KEEP TRACK OF INTERVAL ITERATIONS
+  let intervalCount = 0;
+
+  const interval = setInterval(() => {
+    let randomNum = Math.floor(Math.random() * max);
+
+    //FOR EACH INTERVAL ITERATION, ASSIGN CURRENT RANDOM NUMBER TO HTML ELEMENT FOR 'SLOT MACHINE' EFFECT
+    //INCREMENT INTERVAL COUNT
+    lottoNum.innerHTML = randomNum;
+    intervalCount++;
+
+    // ONCE INTERVAL HAS ITERATED 10 TIMES, ASSIGN LATEST RANDOM NUMBER TO VARIABLE,
+    // CALL DECLARERESULTS FUNCTION TO DISPLAY RESULTS MODAL,
+    // AND CLEAR INTERVAL
+    if (intervalCount === 10) {
+      latestNum = randomNum;
+      declareResults(latestNum);
+      clearInterval(interval);
+    };
+  }, 100);
+};
+
+const declareResults = latestNum => {
+  modal.style = "display: flex";
+
+  if (latestNum > 1000) {
+    result.innerHTML = "Too bad! You've lost!";
+  } else result.innerHTML = "Congratulations! You've won!";
+};
+
+const closeModal = () => {
+  modal.style = "display: none";
 };
